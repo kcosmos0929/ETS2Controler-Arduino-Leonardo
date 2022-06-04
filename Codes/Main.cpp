00c1340f151343aff90a4a8d2dcad76e8248976d
kcosmos0929 <81617707+kcosmos0929@users.noreply.github.com>
2022-06-04 20:12:50 +0900

    Update Main.cpp

--- Codes/Main.cpp
+++ Codes/Main.cpp
@@ -1,49 +1,13 @@
 #include <Arduino.h>
 #include <Joystick.h>
 
-bool TailLightPinState = 0;
-bool HeadLightPinState = 0;
-bool RightBlinkerPinState = 0;
-bool LeftBlinkerPinState = 0;
-bool LoBeamSwitchPinState = 0;
-bool HiBeamSwitchPinState = 0;
-bool LoBeamSignalPinState = 0;
-bool FogLightPinState = 0;
-bool IntWiperPinState = 0;
-bool LoWiperPinState = 0;
-bool HiWiperPinState = 0;
-bool MistWiperPinState = 0;
-bool RearIntWiperPinState = 0;
-bool RearOnWiperPinState = 0;
-bool WasherPinState = 0;
-bool RearWasherPinState = 0;
-bool AnalogIntPinValue = 0;
+bool Light_Tail(0), Light_Head(0), Beam_LO(0), Beam_HI(0), Beam_Sig(0),
+//Light Switch boolean variables
+Blinker_Right(0), Blinker_Left(0),
+//Blinker boolean variables
+FWiper_INT(0), FWiper_LO(0), Fwiper_HI(0), RWiper_INT(0), RWiper_ON(0),
+//Wiper Switch boolean variables
+Fog(0), Mist(0), FWasher(0), RWasher(0);
 
-// Define the variables of states and set the variables to zero
+int HeadLamp, Beam, Blinker, FWiper, RWiper;
 
-int Value;
-int S1;
-int S2;
-int Data;
-int SH;
-int SP;
-
-// Define the pinS
-
-char HeadLampSwitch = "O";
-// O = off, T = tail, H = head
-char BlinkerSwitch = "O";
-// O = off, R = right, L = left
-char PassingDimmerSwitch = "L";
-// L = low, H = high, P = passing
-char FogLampSwitch = "F";
-// F = off, T = on
-char WiperSwitch = "O";
-// O = off, I = int, L = lo, H = hi
-char MistSwitch = "F";
-// F = off, T = on
-char RearWiperSwitch = "O";
-// O = off, I = int, N = on
-char WasherSwitch = "F";
-char RearWasherSwitch = "F";
-// F = off, T = on
